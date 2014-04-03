fc790ad12c192de53f7b6a72c933052e51b654c2
Thomas Kliszowski <thomas@stupeflix.com>
2014-04-03 14:40:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tip
+
+## Description:
+
+Tip is a plugin which allows you to add some tips, easily!
+
+## Usage:
+
+Initialise Tip:
+    <script>
+    $(document).ready(function(){
+      $('[data-tip]').tip({
+        delay: 0,
+        margin: 2
+      });
+    });
+    </script>
---
+++ jquery.tip.js
@@ -0,0 +1,81 @@
+(function(jQuery){
+  var Tip = function(target, options){
+    this.target = target;
+    this.options = $.extend({
+      margin: 2,
+      delay: 0
+    }, options);
+
+    this.tip = $('<div class="widget-tip">');
+    this.text = $('<div class="widget-tip-text">');
+    this.pointer = $('<div class="widget-tip-pointer">');
+    this.tip.append(this.text).append(this.pointer);
+
+    this.target.hover($.proxy(this.show, this), $.proxy(this.hide, this));
+  };
+
+  Tip.prototype.show = function(){
+    var target = this.target;
+    var text = this.text;
+    var tip = this.tip;
+    var options = this.options;
+
+    var html = target.attr('data-tip');
+
+    tip.clearQueue().delay(options.delay).queue(function(){
+      tip.removeClass('animate').removeClass('active');
+      $(this).dequeue();
+    }).delay(50).queue(function(){
+      text.html(html);
+      $('body').append(tip);
+
+      // Move tip
+      var offset = $(target).offset();
+      var top = offset.top + $(target).outerHeight() + options.margin;
+      var left = offset.left + ($(target).outerWidth() / 2) - ($(tip).outerWidth() / 2);
+
+      tip.css({
+        'top': top,
+        'left': left
+      });
+
+      $(this).dequeue();
+    }).delay(0).queue(function(){
+      tip.addClass('animate');
+      $(this).dequeue();
+    }).delay(50).queue(function(){
+      tip.addClass('active');
+      $(this).dequeue();
+    }).delay(224).queue(function(){
+      tip.removeClass('animate');
+      $(this).dequeue();
+    });
+  };
+
+  Tip.prototype.hide = function(){
+    var text = this.text;
+    var tip = this.tip;
+
+    tip.clearQueue().delay(0).queue(function(){
+      tip.addClass('animate');
+      $(this).dequeue();
+    }).delay(50).queue(function(){
+      tip.removeClass('active');
+      $(this).dequeue();
+    }).delay(224).queue(function(){
+      tip.removeClass('animate');
+      tip.detach();
+      $(this).dequeue();
+    });
+  };
+
+  jQuery.fn.tip = function(options){
+    if(typeof options != typeof {}) option = {};
+
+    $(this).each(function(){
+      if($(this).attr('data-tip')){
+        new Tip($(this), options);
+      }
+    });
+  };
+})(jQuery);
